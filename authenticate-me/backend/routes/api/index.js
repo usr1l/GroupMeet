5cb5ef0c4b665a0cfe0492224a1a20e94f6cd570
TKNY <zhtony0398@gmail.com>
2022-12-16 18:30:08 -0500

    phase3

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -4,42 +4,9 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 
-// router.post('/test', function (req, res) {
-//   res.json({ requestBody: req.body });
-// });
-
-// GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
-});
-
-router.get(
-  '/restore-user',
-  restoreUser,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
-// GET /api/restore-user
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
 router.use(restoreUser);
-
-const { requireAuth } = require('../../utils/auth.js');
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
 
 module.exports = router;
