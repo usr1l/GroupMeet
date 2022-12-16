bb6604e0976e961a8d1425ca713d7a5f21085d4f
TKNY <zhtony0398@gmail.com>
2022-12-16 16:05:17 -0500

    add user model aethentication sequelize scopes and methods

--- authenticate-me/backend/db/migrations/20221215224434-create-user.js
+++ authenticate-me/backend/db/migrations/20221215224434-create-user.js
@@ -1,6 +1,7 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 
+// need this in all seeder and migration files
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,8 +1,12 @@
+// backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
-router.post('/test', function (req, res) {
-  res.json({ requestBody: req.body });
-});
+// router.post('/test', function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
 
 module.exports = router;
