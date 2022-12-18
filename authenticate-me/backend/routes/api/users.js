7916ab9e4bdb2d8350a9f5f0496558c88aa3e7c4
TKNY <zhtony0398@gmail.com>
2022-12-18 15:58:09 -0500

    signup error

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -9,6 +9,12 @@ const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 const validateSignup = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide your first name.'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide your last name.'),
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
@@ -34,7 +40,7 @@ router.post(
   validateSignup,
   async (req, res) => {
     const { firstName, lastName, email, password, username } = req.body;
-    const user = await User.signup({ email, username, password });
+    const user = await User.signup({ firstName, lastName, email, username, password });
 
     await setTokenCookie(res, user);
 
