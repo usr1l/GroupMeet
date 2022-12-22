c26014f957f15d72911bcf649783b8f14875b999
TKNY <zhtony0398@gmail.com>
2022-12-22 09:16:54 -0500

    seeded

--- authenticate-me/backend/db/seeders/20221222124211-demo-attendance.js
+++ authenticate-me/backend/db/seeders/20221222124211-demo-attendance.js
@@ -1,25 +1,38 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    options.tableName = 'Attendances';
+    await queryInterface.bulkInsert(options, [
+      {
+        status: 'waitlist',
+        eventId: 3,
+        userId: 2
+      },
+      {
+        status: 'co-host',
+        eventId: 1,
+        userId: 1
+      },
+      {
+        status: 'co-host',
+        eventId: 2,
+        userId: 3
+      },
+    ])
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Attendances';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options, [
+      { id: { [Op.in]: [1, 2, 3] } }
+    ])
   }
 };
--- authenticate-me/backend/db/seeders/20221222124220-demo-membership.js
+++ authenticate-me/backend/db/seeders/20221222124220-demo-membership.js
@@ -1,25 +1,43 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    options.tableName = 'Memberships'
+    await queryInterface.bulkInsert(options, [
+      {
+        status: 'co-host',
+        userId: 1,
+        groupId: 1
+      },
+      {
+        status: 'co-host',
+        userId: 2,
+        groupId: 2
+      },
+      {
+        status: 'co-host',
+        userId: 3,
+        groupId: 3
+      },
+      {
+        status: 'member',
+        userId: 2,
+        groupId: 1
+      },
+    ])
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Memberships';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options, [
+      { id: { [Op.in]: [1, 2, 3, 4] } }
+    ])
   }
 };
