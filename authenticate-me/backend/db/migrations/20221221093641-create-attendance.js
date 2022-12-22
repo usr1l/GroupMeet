8c9242e6cfa530086f2edf06a177ec8c3ba6a4e3
TKNY <zhtony0398@gmail.com>
2022-12-22 09:53:19 -0500

    seeder-edit

--- authenticate-me/backend/db/migrations/20221221093641-create-attendance.js
+++ authenticate-me/backend/db/migrations/20221221093641-create-attendance.js
@@ -5,11 +5,12 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
+options.tableName = 'Attendances';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Attendances', {
+    await queryInterface.createTable(options, {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -47,6 +48,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Attendances');
+    await queryInterface.dropTable(options);
   }
 };
