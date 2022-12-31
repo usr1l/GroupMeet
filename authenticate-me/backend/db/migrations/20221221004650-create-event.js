9061382c18f6353661bf4f6cc6cdd0bfe60dd01f
TKNY <zhtony0398@gmail.com>
2022-12-31 09:00:36 -0500

    change price to decimal

--- authenticate-me/backend/db/migrations/20221221004650-create-event.js
+++ authenticate-me/backend/db/migrations/20221221004650-create-event.js
@@ -34,7 +34,7 @@ module.exports = {
         // allowNull: false
       },
       price: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.DECIMAL,
         // allowNull: false
       },
       startDate: {
