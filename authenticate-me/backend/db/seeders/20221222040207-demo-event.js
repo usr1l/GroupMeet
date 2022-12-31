9887924087c7220b315557dc623a440bdffe406d
TKNY <zhtony0398@gmail.com>
2022-12-31 12:55:14 -0500

    import op

--- authenticate-me/backend/db/seeders/20221222040207-demo-event.js
+++ authenticate-me/backend/db/seeders/20221222040207-demo-event.js
@@ -50,6 +50,7 @@ module.exports = {
 
   async down(queryInterface, Sequelize) {
     options.tableName = 'Events';
+    const { Op } = require('sequelize');
     return queryInterface.bulkDelete(options, {
       name: { [Op.in]: ['Event1', 'Event2', 'Event3'] }
     }, {});
