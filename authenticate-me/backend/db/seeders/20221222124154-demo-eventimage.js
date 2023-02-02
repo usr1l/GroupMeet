7187be32a7f162dc40b32099d5fc008abee7f6c5
TKNY <zhtony0398@gmail.com>
2023-02-02 06:16:43 -0500

    demos

--- authenticate-me/backend/db/seeders/20221222124154-demo-eventimage.js
+++ authenticate-me/backend/db/seeders/20221222124154-demo-eventimage.js
@@ -9,8 +9,8 @@ if (process.env.NODE_ENV === 'production') {
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-
-
+  async up(queryInterface, Sequelize) {
+  },
   async down(queryInterface, Sequelize) {
     options.tableName = 'EventImages';
     const { Op } = require('sequelize');
