ffc271edaa276570f5922ea6625353b7a032dd98
TKNY <zhtony0398@gmail.com>
2023-06-25 03:34:38 -0400

    latest

--- authenticate-me/frontend/src/components/HomePage/index.js
+++ authenticate-me/frontend/src/components/HomePage/index.js
@@ -8,7 +8,27 @@ const HomePage = () => {
     <>
       <FeaturesBar />
       <div id='homepage-welcome'>
-        Welcome to GroupMeet!
+        <div>
+
+          Welcome to GroupMeet!
+        </div>
+        <br />
+        <br />
+        <br />
+        <div id='links'>
+          <div>Tony Zheng</div>
+          <div id='link-icons'>
+            <a href="https://www.linkedin.com/in/tony-zheng-577840156/" target="_blank">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/usr1l" target="_blank">
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="https://usr1l.github.io/#" target="_blank">
+              <i class="fa-solid fa-globe"></i>
+            </a>
+          </div>
+        </div>
       </div>
     </>
   )
